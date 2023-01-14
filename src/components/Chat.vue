434071e14e75fbaa1e7f8cea3539162a3421be85
MarioFBSantos <mariosantos3197@gmail.com>
2023-01-14 19:39:06 -0300

    changed again the first page and created new routes

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,150 +1,181 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <template>
-  <div>
-    <div>
-      <div class="q-pa-md">
-        <q-btn label="Open Dialog" color="primary" @click="dialog = true" />
+	<div>
+		<div>
+			<div class="q-pa-md">
+				<q-dialog
+					v-model="dialog"
+					persistent
+					transition-show="flip-down"
+					transition-hide="flip-up"
+					:maximized="maximizedToggle"
+				>
+					<q-card>
+						<q-bar>
+							<q-space />
+							<q-btn
+								dense
+								flat
+								icon="minimize"
+								@click="maximizedToggle = false"
+								:disable="!maximizedToggle"
+							>
+								<q-tooltip
+									v-if="maximizedToggle"
+									class="bg-white text-primary"
+									>Minimize</q-tooltip
+								>
+							</q-btn>
+							<q-btn
+								dense
+								flat
+								icon="crop_square"
+								@click="maximizedToggle = true"
+								:disable="maximizedToggle"
+							>
+								<q-tooltip
+									v-if="!maximizedToggle"
+									class="bg-white text-primary"
+									>Maximize</q-tooltip
+								>
+							</q-btn>
+							<q-btn dense flat icon="close" to="/">
+								<q-tooltip class="bg-white text-primary"
+									>Close</q-tooltip
+								>
+							</q-btn>
+						</q-bar>
+						<q-card-section class="row items-center">
+							<Display
+								:messages="messages"
+								class="display"
+							></Display>
+						</q-card-section>
 
-        <q-dialog
-          v-model="dialog"
-          persistent
-          transition-show="flip-down"
-          transition-hide="flip-up"
-          :maximized="maximizedToggle"
-        >
-          <q-card>
-            <q-bar>
-              <q-space />
-
-              <q-btn
-                dense
-                flat
-                icon="minimize"
-                @click="maximizedToggle = false"
-                :disable="!maximizedToggle"
-              >
-                <q-tooltip v-if="maximizedToggle" class="bg-white text-primary"
-                  >Minimize</q-tooltip
-                >
-              </q-btn>
-              <q-btn
-                dense
-                flat
-                icon="crop_square"
-                @click="maximizedToggle = true"
-                :disable="maximizedToggle"
-              >
-                <q-tooltip v-if="!maximizedToggle" class="bg-white text-primary"
-                  >Maximize</q-tooltip
-                >
-              </q-btn>
-              <q-btn dense flat icon="close" v-close-popup>
-                <q-tooltip class="bg-white text-primary">Close</q-tooltip>
-              </q-btn>
-            </q-bar>
-            <q-card-section class="row items-center">
-              <Display :messages="messages" class="display"></Display>
-            </q-card-section>
-
-            <q-card-section class="row items-center">
-              <q-input
-                outlined
-                v-model="text"
-                label="Type here"
-                @keydown.enter="insertUserMsg(text)"
-                class="chatInput q-mt-xs"
-                :disable="input"
-              />
-            </q-card-section>
-
-            <!-- Notice v-close-popup -->
-          </q-card>
-        </q-dialog>
-      </div>
-    </div>
-  </div>
+						<q-card-section class="row items-center">
+							<q-input
+								outlined
+								v-model="text"
+								label="Type here"
+								@keydown.enter="insertUserMsg(text)"
+								class="chatInput q-mt-xs"
+								:disable="input"
+							/>
+						</q-card-section>
+					</q-card>
+				</q-dialog>
+			</div>
+		</div>
+	</div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue';
-import iMessage from './iMessage';
-import Display from '../components/display.vue';
+	import { defineComponent, ref, reactive } from 'vue';
+	import iMessage from './iMessage';
+	import Display from '../components/display.vue';
 
-export default defineComponent({
-  name: 'chat',
-  setup() {
-    const messages = ref<iMessage[]>([
-      {
-        name: 'you',
-        message: 'Ask something',
-        textcolor: 'white',
-        bgcolor: 'primary',
-      },
-    ]);
-    return {
-      text: ref(''),
-      messages,
-      dialog: ref(false),
-      maximizedToggle: ref(false),
-      input: ref(false),
-    };
-  },
-  components: { Display },
-  updated() {
-    // this.scrollToEnd();
-  },
-  methods: {
-    async insertUserMsg(msg: string) {
-      const obj = reactive({
-        name: 'you',
-        message: msg,
-        textcolor: 'white',
-        bgcolor: 'primary',
-      });
+	export default defineComponent({
+		name: 'chat',
+		props: ['openModal'],
+		setup() {
+			const messages = ref<iMessage[]>([
+				{
+					name: 'you',
+					message: 'Ask something',
+					textcolor: 'white',
+					bgcolor: 'primary',
+				},
+			]);
 
-      this.messages.push(obj);
-      await this.sendChat();
-      this.text = '';
-    },
+			return {
+				text: ref(''),
+				messages,
+				dialog: ref(true),
+				dialogFlag: ref(false),
+				maximizedToggle: ref(false),
+				input: ref(false),
+			};
+		},
+		components: { Display },
+		methods: {
+			handleDialog() {
+				!this.dialogFlag;
+			},
+			async insertUserMsg(msg: string) {
+				const obj = reactive({
+					name: 'you',
+					message: msg,
+					textcolor: 'white',
+					bgcolor: 'primary',
+				});
 
-    insertBotMsg(msg: string) {
-      try {
-        console.log(msg);
-        this.messages.push({
-          name: 'bot',
-          message: msg,
-          textcolor: 'gray',
-          bgcolor: 'active',
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    },
+				this.messages.push(obj);
+				await this.sendChat();
+				this.text = '';
+			},
 
-    // scrollToEnd() {
-    //   const content = this.$refs.container;
-    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //   // @ts-ignore
-    //   content.scrollTop = content.scrollHeight;
-    // },
+			insertBotMsg(msg: string) {
+				try {
+					this.messages.push({
+						name: 'bot',
+						message: msg,
+						textcolor: 'white',
+						bgcolor: 'purple',
+					});
+				} catch (err) {
+					console.log(err);
+				}
+			},
 
-    async sendChat() {
-      try {
-        this.input = true;
-        const { data } = await this.$axios.post('http://localhost:8000', {
-          question: this.text,
-        });
-        this.text = '';
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-        this.insertBotMsg(data.bot);
-        this.input = false;
-      } catch (err) {
-        console.log(err);
-        this.input = false;
-      }
-    },
-  },
-  mounted() {
-    // this.scrollToEnd();
-  },
-});
+			async sendChat() {
+				try {
+					this.input = true;
+					const { data } = await this.$axios.post(
+						'https://chat-ai-backend.vercel.app',
+						{
+							question: this.text,
+						}
+					);
+					this.text = '';
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+					this.insertBotMsg(data.bot);
+					this.input = false;
+				} catch (err) {
+					console.log(err);
+					this.input = false;
+				}
+			},
+		},
+	});
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.chatInput {
+		width: 100%;
+	}
+
+	@media screen and (max-width: 850px) {
+		.button {
+			width: 150px;
+			height: 50px;
+			border-radius: 8px;
+			background-color: black;
+			border: none;
+			color: white;
+
+			margin-top: 15px;
+			font-size: 1.2em;
+		}
+	}
+	@media screen and (min-width: 851px) {
+		.button {
+			width: 150px;
+			height: 50px;
+			border-radius: 8px;
+			background-color: black;
+			border: none;
+			color: white;
+
+			margin-top: 15px;
+			font-size: 1.2em;
+		}
+	}
+</style>
